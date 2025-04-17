f44f25bc7c9eb7dc9f68235b3daca4029c0848eb
Raha Heidari <heidariraha761@gmail.com>
2025-04-17 12:55:56 -0700

    quiz2

---
+++ quiz2.js
@@ -0,0 +1,21 @@
+/*
+Question2
+Create a Node.js script that reads the content of a file named 'example.txt' using the fs.readFile() function. Ensure that the file is read with the 'utf8' encoding.
+
+- Implement error handling to log any errors encountered during the file reading process.
+- If the file is successfully read, display its content to the console along with the message "File content:".
+- Your solution should include the usage of the fs.readFile() function
+*/
+
+// answer
+const fs=require("fs");
+
+fs.readFile("./example.txt","utf-8",(error,data)=>{
+
+    if(error){
+        console.log(error);
+    }else{
+        console.log("file content:" + data);
+        
+    }
+})
